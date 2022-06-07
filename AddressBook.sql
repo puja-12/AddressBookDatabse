25be422abf9a240770b7949b9c680edd19f65d10
puja <pujarana1796@gmail.com>
2022-06-07 13:40:13 +0530

    Creating a table addressbook

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,13 @@
 Create database Addressbook_system
 use Addressbook_system
+
+create Table Addressbook
+(
+Firstname varchar(20),
+Lastname varchar(20),
+Address varchar (100),
+City varchar(20),
+State varchar(20),
+Zip int,
+Email varchar(20),
+);
